c3e4496963f466b9e786f4f30b192644d18ebe18
Edgar Mevaa <mevaed4@gmail.com>
2026-07-01 11:06:31 +0100

    Ci: Testing the CI

--- infra/powerbi/secrets/main.tf
+++ infra/powerbi/secrets/main.tf
@@ -1,14 +1,12 @@
 resource "aws_secretsmanager_secret" "powerbi_credentials" {
-  count = var.enabled ? 1 : 0
-
   name        = "${var.project_name}-powerbi-credentials"
   description = "Power BI OAuth application credentials"
   tags        = var.tags
 }
 
 resource "aws_secretsmanager_secret_version" "powerbi_credentials" {
-  count = var.enabled? 1 : 0
+  count = var.enabled ? 1 : 0
 
-  secret_id     = aws_secretsmanager_secret.powerbi_credentials[0].id
+  secret_id     = aws_secretsmanager_secret.powerbi_credentials.id
   secret_string = var.powerbi_credentials_json
 }
--- infra/powerbi/secrets/output.tf
+++ infra/powerbi/secrets/output.tf
@@ -1,13 +1,13 @@
 output "powerbi_credentials_secret_arn" {
   value = try(
-    aws_secretsmanager_secret.powerbi_credentials[0].arn,
+    aws_secretsmanager_secret.powerbi_credentials.arn,
     null
   )
 }
 
 output "powerbi_credentials_secret_name" {
   value =  try(
-    aws_secretsmanager_secret.powerbi_credentials[0].name,
+    aws_secretsmanager_secret.powerbi_credentials.name,
     null
   )
 }
