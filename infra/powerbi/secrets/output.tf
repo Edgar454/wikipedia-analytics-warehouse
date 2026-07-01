c2edacabf566355ed3835043e679ac920c9bc417
Edgar Mevaa <mevaed4@gmail.com>
2026-07-01 10:11:38 +0100

    Ci: Testing the CI

--- infra/powerbi/secrets/output.tf
+++ infra/powerbi/secrets/output.tf
@@ -1,11 +1,7 @@
 output "powerbi_credentials_secret_arn" {
-  value = length(aws_secretsmanager_secret.powerbi_credentials) > 0
-    ? aws_secretsmanager_secret.powerbi_credentials[0].arn
-    : null
+  value = length(aws_secretsmanager_secret.powerbi_credentials) > 0 ? aws_secretsmanager_secret.powerbi_credentials[0].arn : null
 }
 
 output "powerbi_credentials_secret_name" {
-  value = length(aws_secretsmanager_secret.powerbi_credentials) > 0
-    ? aws_secretsmanager_secret.powerbi_credentials[0].name
-    : null
+  value = length(aws_secretsmanager_secret.powerbi_credentials) > 0 ? aws_secretsmanager_secret.powerbi_credentials[0].name : null
 }
